edf7c8a8e11ee56774c78251aa1fe44ecb2e4d94
hoonch3 <hoony@member.fsf.org>
2016-04-11 00:27:18 +0900

    fix bug

--- app/controllers/api/official/official.js
+++ app/controllers/api/official/official.js
@@ -4,7 +4,46 @@ var db = require('../../../models')
 module.exports = function() {
   var router = express.Router()
 
-  router.get('/', function(req, res, next) {
+  router.get('/:uniqueId', function (req, res, next) {
     var uniqueId = req.params.uniqueId
+
+    db.Official.findAll({
+      order: [
+        ['year', 'ASC']
+      ],
+      include: [{
+        model: db.Person,
+        attributes: ['uniqueId', 'name'],
+        where: { uniqueId: uniqueId }
+      }, {
+        model: db.Position,
+        attributes: ['title'],
+        include: [{
+          model: db.Org3,
+          attribute: ['title'],
+          incude: [{
+            model: db.Org2,
+            attribute: ['title'],
+            include: [{
+              model: db.Org1,
+              attribute: ['title']
+            }]
+          }]
+        }]
+      }, {
+        model: db.Asset,
+        include: [{
+          model: db.Cat2,
+          include: [{
+            model: db.Cat1
+          }]
+        }]
+      }]
+    })
+    .then(function (officials) {
+      res.json(officials)
+    })
+  })
+
   return router
 }
--- app/views/app/scripts/views/searchbox.js
+++ app/views/app/scripts/views/searchbox.js
@@ -286,7 +286,9 @@ Officials.Views = Officials.Views || {};
 
 
       // set current url with query parameters
-      // Backbone.history.navigate('?' + this.fixEncodeURI($.param(params)))
+      if (Backbone.history.getFragment().split('?')[0] !== "") {
+        Backbone.history.navigate('?' + this.fixEncodeURI($.param(params)))
+      }
       // find results
       this.getResult(params)
     },
@@ -317,7 +319,9 @@ Officials.Views = Officials.Views || {};
       params.election = 1
 
       // set current url with query parameters
-      // Backbone.history.navigate('?' + this.fixEncodeURI($.param(params)))
+      if (Backbone.history.getFragment().split('?')[0] !== "") {
+        Backbone.history.navigate('?' + this.fixEncodeURI($.param(params)))
+      } 
       // find results
       this.getResult(params)
     },
